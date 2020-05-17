420902b704bea81fc87993239ae2432e78b9da30
derozio <paradoxcrusher@gmail.com>
2020-05-17 17:06:50 +0530

    GG-2: Added LoadNextScene, improved LevelLoader

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,14 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
     void Start()
     {
-        StartCoroutine(StartMenuScreenAfterDelay());
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(StartMenuScreenAfterDelay());
+        }        
     }
 
     IEnumerator StartMenuScreenAfterDelay()
     {
-        yield return new WaitForSeconds(4f);
-        SceneManager.LoadScene(1);
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
