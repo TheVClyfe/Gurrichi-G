dc4a1171e392c6498d10ffed5fc08cdf1f75293d
derozio <paradoxcrusher@gmail.com>
2020-05-17 16:58:43 +0530

    GG-1: First Commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(StartMenuScreenAfterDelay());
+    }
+
+    IEnumerator StartMenuScreenAfterDelay()
+    {
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene(1);
+    }
+}
